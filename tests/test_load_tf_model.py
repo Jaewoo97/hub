c462fc79cd1942a1a8933ebdb34570e32116e311
marcus neo <81731201+marcus-datature@users.noreply.github.com>
2021-05-10 14:26:00 +0800

    Update test_load_tf_model.py
    Support the class version of HubModel

--- tests/test_load_tf_model.py
+++ tests/test_load_tf_model.py
@@ -10,12 +10,23 @@ def test_load_tf_model(tmpdir, melanoma_model):
     if os.environ.get("HUB_CUSTOM_ENDPOINT", None) is not None:
         hub._set_hub_endpoint(os.environ["HUB_CUSTOM_ENDPOINT"])
 
-    model_key, project_secret, images, image_size, threshold, predictions = \
-        melanoma_model
+    (
+        model_key,
+        project_secret,
+        images,
+        image_size,
+        threshold,
+        predictions,
+    ) = melanoma_model
 
-    trained_model = hub.load_tf_model(model_key, project_secret, str(tmpdir))
+    hub_model = hub.HubModel(
+        model_key=model_key, project_secret=project_secret
+    )
 
-    actual_predictions = predict.run_prediction(trained_model, images,
-                                                image_size, threshold)
+    trained_model = hub_model.load_tf_model(model_cache_dir=str(tmpdir))
+
+    actual_predictions = predict.run_prediction(
+        trained_model, images, image_size, threshold
+    )
 
     assert predict.predictions_equal(actual_predictions, predictions)
